26fd895256d76a2e082f2a2e16a60aedcfca44cb
abudaan <daniel@abumarkub.net>
2020-05-06 18:23:04 +0200

    update portfolio

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,103 +1,125 @@
-import React, {Component} from 'react'
-import {Container} from 'flux/utils'
-import Actions from '../actions'
-import Contact from '../components/contact'
-import Menu from '../components/menu'
-import ProjectInfo from '../components/project_info'
-import ImageSlider from '../components/image_slider'
-import Store from '../store'
-import * as DisplayStates from '../constants/display_states'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import React, { Component } from "react";
+import { Container } from "flux/utils";
+import Actions from "../actions";
+import Contact from "../components/contact";
+import Menu from "../components/menu";
+import ProjectInfo from "../components/project_info";
+import ImageSlider from "../components/image_slider";
+import Store from "../store";
+import * as DisplayStates from "../constants/display_states";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
-
-class App extends Component{
-
-  static displayName = 'App'
+class App extends Component {
+  static displayName = "App";
 
   static getStores() {
     return [Store];
   }
 
-  static calculateState(prevState){
+  static calculateState(prevState) {
     //console.log(prevState)
-    return {...Store.getState()}
+    return { ...Store.getState() };
   }
 
-  constructor(){
-    super()
-    this.index = Store.getState().index
-    this._windowResizeListener = this._handleResize.bind(this)
-    this._windowKeyPressListener = this._handleKeyPress.bind(this)
-    this._orientationChangeListener = this._handleOrientationChange.bind(this)
-    this._swipeListener = this._handleSwipe.bind(this)
-    Actions.loadData()
+  constructor() {
+    super();
+    this.index = Store.getState().index;
+    this._windowResizeListener = this._handleResize.bind(this);
+    this._windowKeyPressListener = this._handleKeyPress.bind(this);
+    this._orientationChangeListener = this._handleOrientationChange.bind(this);
+    this._swipeListener = this._handleSwipe.bind(this);
+    Actions.loadData();
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this._windowKeyPressListener)
-    document.addEventListener('swipe', this._swipeListener)
+    document.addEventListener("keydown", this._windowKeyPressListener);
+    document.addEventListener("swipe", this._swipeListener);
     // if(this.state.os !== 'ios'){
     //   window.addEventListener('resize', this._windowResizeListener)
     // }
     // NOTE: resize listneren is necessary on ios when the address bar appears or disappears!
-    window.addEventListener('resize', this._windowResizeListener)
-    window.addEventListener('orientationchange', this._orientationChangeListener)
-    Actions.startAutoSlider()
+    window.addEventListener("resize", this._windowResizeListener);
+    window.addEventListener(
+      "orientationchange",
+      this._orientationChangeListener
+    );
+    Actions.startAutoSlider();
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this._windowKeyPressListener)
-    document.removeEventListener('swipe', this._swipeListener)
+    document.removeEventListener("keydown", this._windowKeyPressListener);
+    document.removeEventListener("swipe", this._swipeListener);
     // if(this.state.os !== 'ios'){
     //   window.removeEventListener('resize', this._windowResizeListener)
     // }
-    window.removeEventListener('resize', this._windowResizeListener)
-    window.removeEventListener('orientationchange', this._orientationChangeListener)
+    window.removeEventListener("resize", this._windowResizeListener);
+    window.removeEventListener(
+      "orientationchange",
+      this._orientationChangeListener
+    );
   }
 
-  _handleResize(){
+  _handleResize() {
     //document.getElementById('debug').innerHTML = window.innerWidth + 'x' + window.innerHeight
-    Actions.setSize()
+    Actions.setSize();
   }
 
-  _handleKeyPress(e){
-    Actions.sliderKeyPress(e)
+  _handleKeyPress(e) {
+    Actions.sliderKeyPress(e);
   }
 
-  _handleSwipe(e){
-    Actions.sliderSwiped(e)
+  _handleSwipe(e) {
+    Actions.sliderSwiped(e);
   }
 
-  _handleOrientationChange(e){
-    Actions.setOrientation()
+  _handleOrientationChange(e) {
+    Actions.setOrientation();
   }
 
-  render(){
-    let component = null
+  render() {
+    let component = null;
 
     //console.log(this.state)
 
-    switch(this.state.displayState){
-
+    switch (this.state.displayState) {
       case DisplayStates.WARNING:
-        return <div key={'warning'} className="message">{this.state.message}</div>
+        return (
+          <div key={"warning"} className="message">
+            {this.state.message}
+          </div>
+        );
 
       case DisplayStates.MESSAGE:
         // return <div key={'loading'} className="message">{this.state.message}</div>
-        component = <div key={'loading'} className="message">{this.state.message}</div>
-        break
+        component = (
+          <div key={"loading"} className="message">
+            {this.state.message}
+          </div>
+        );
+        break;
 
       case DisplayStates.MAIN:
-        component = <ImageSlider key={'imageslider'} {...this.state} sliderClicked={Actions.sliderClicked} />
-        break
+        component = (
+          <ImageSlider
+            key={"imageslider"}
+            {...this.state}
+            sliderClicked={Actions.sliderClicked}
+          />
+        );
+        break;
 
       case DisplayStates.CONTACT:
-        component = <Contact key={'contact'} {...this.state} hideContact={Actions.hideContact}/>
-        break
+        component = (
+          <Contact
+            key={"contact"}
+            {...this.state}
+            hideContact={Actions.hideContact}
+          />
+        );
+        break;
 
       default:
-        // let's take a walk in the woods
-
+      // let's take a walk in the woods
     }
 
     return (
@@ -109,8 +131,8 @@ class App extends Component{
           menuClicked={Actions.menuClicked}
         />
         <ReactCSSTransitionGroup
-          component={'div'}
-          transitionName={'component'}
+          component={"div"}
+          transitionName={"component"}
           transitionAppear={true}
           transitionAppearTimeout={500}
           transitionEnterTimeout={500}
@@ -125,8 +147,8 @@ class App extends Component{
           />
         </ReactCSSTransitionGroup>
       </div>
-    )
+    );
   }
 }
 
-export default Container.create(App)
+export default Container.create(App);
